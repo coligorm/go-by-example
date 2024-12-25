3bfad8fb3c3569611c6e274dcbdef8fdcfbc7223
coligorm <colinrhysgorman@gmail.com>
2024-12-25 17:20:55 +1300

    Added error handing to hello.go and greetings.go

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,15 +1,25 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	// import error module for error handling
+	"errors"
+)
 
 // Hello returns a greeting for the named person.
 
 // Similar to java, state parameter type and return type
 // when creating a function
 // Function names get captial letters
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name is given, return an error with a message
+	if name == "" {
+		// New returns an error that formats as the given text
+		return "", errors.New("empty name")
+	}
 	// Sprintf is a formatted string function which takes the variable
 	// name, and inputs it into the %v, verb, position
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,11 @@
 // a package is a way to group functions, and it's made up of all the files in the same directory
 package main
 
-// contains functions for formatting text
 import (
+	// contains functions for formatting text
 	"fmt"
+	// implements a simple logging package
+	"log"
 
 	"rsc.io/quote"
 
@@ -19,11 +21,32 @@ func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
 
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	// passing the name "coligorm" into the Hello function
 	// from the greetings module
 
 	// I need to Edit the go-by-example/hello module to use my local go-by-example/greetings module.
 	// by running go mod edit -replace go-by-example/greetings=../greetings
-	message := greetings.Hello("coligorm")
-	fmt.Println(message)
+	name_message, err := greetings.Hello("coligorm")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(name_message)
+
+	// Request a error greeting message.
+	blank_message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(blank_message)
 }
