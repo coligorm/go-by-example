057c7d53f853333b07ac8203df5046fb22d3f441
coligorm <colinrhysgorman@gmail.com>
2024-12-26 14:20:13 +1300

    Updated files with slice and random number generator

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 # go-by-example
 repo to learn Go using site [GoByExample](https://gobyexample.com/)
 
-## Notes
+## CMD Line Notes
 - add go.mod to each directory containing the go code by running:  
 `go mod init [module/path]`  
 - Update go.mod requirements with:  
 `go mod tidy`
-- 
+- In order to use your own local module, you have to redirect  Go tools from the incorrent module path to the local one:  
+`go mod edit -replace go-by-example/greetings=../greetings`
+
+## Code Notes
+- `:-` is used to declare and assign a variable in one line
+- Function names get capital letters, param types and return types  
+`func Example(param string) string`
+- A slice is the same as an array, who's size can change dynamically when adding or removing items  
+`example_list_of_strings := []string`
+- rand.Intn is used to return an int between 0:n  
+`rand.Intn(len(example_list_of_strings))`
+
 ## Popular Import List
-- import "fmt"
+- "fmt"
+- "error"
+- log
+- "math/rand" (random number generator module)
--- greetings/greetings.go
+++ greetings/greetings.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	// import error module for error handling
 	"errors"
+	// rand implements pseudo-random number generators
+	"math/rand"
 )
 
 // Hello returns a greeting for the named person.
@@ -19,7 +21,26 @@ func Hello(name string) (string, error) {
 	}
 	// Sprintf is a formatted string function which takes the variable
 	// name, and inputs it into the %v, verb, position
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	// Call randomFormat to return a random format greeting
+	message := fmt.Sprintf(randomFormat(), name)
 
 	return message, nil
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	// A slice is the same as an array, who's size changes dynamically
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+		"Hello there, %v",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	// Using rand.Intn, returns an int between 0:n (n being the length of the slice)
+	return formats[rand.Intn(len(formats))]
 }
